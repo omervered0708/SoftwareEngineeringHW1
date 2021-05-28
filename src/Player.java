d252645f755a9f2d15987a175d537d802bc88184
omervered0708 <omer.vered@campus.technion.ac.il>
2021-05-28 15:02:46 +0300

    added player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,52 @@
 public class Player {
+    private String name;
+    private Deck gameDeck;
+    private Deck prizeDeck;
+
+    public Player(String name) {
+        this.name = name;
+        gameDeck = new Deck(false);
+        prizeDeck = new Deck(false);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Deck getGameDeck() {
+        return gameDeck;
+    }
+
+    public void setGameDeck(Deck gameDeck) {
+        this.gameDeck = gameDeck;
+    }
+
+    public Deck getPrizeDeck() {
+        return prizeDeck;
+    }
+
+    public void setPrizeDeck(Deck prizeDeck) {
+        this.prizeDeck = prizeDeck;
+    }
+     public void addToGameDeck(Card card)
+     {
+         gameDeck.addCard(card);
+     }
+    public void addToPrizeDeck(Card card)
+    {
+        prizeDeck.addCard(card);
+    }
+     public Card drawCard()
+     {
+         if(gameDeck.isEmpty()){
+             prizeDeck.shuffle();
+             gameDeck
+         }
+
+
+     }
 }
