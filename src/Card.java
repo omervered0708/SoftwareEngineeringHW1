5f4d1594b466bc17369f1b8e2b875b98da5ddbfc
omervered0708 <omer.vered@campus.technion.ac.il>
2021-05-28 14:25:14 +0300

    added shape enum

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,38 @@
-public class Card {
+public class Card
+{
+    private int number;
+    private Shape shape;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
+    public Card(int number, Shape shape) {
+        this.number = number;
+        this.shape = shape;
+    }
+    public int compare(Card  other)
+    {
+        if(this.number < other.number)
+            return -1;
+        if(this.number == other.number)
+            return 0;
+        return 1;
+    }
+    private char getShapeCharacter(){
+
+    }
+
 }
